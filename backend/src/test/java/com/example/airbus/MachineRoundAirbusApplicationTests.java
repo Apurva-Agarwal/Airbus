89c1ce2a6c2ea2e782932b1bb08933e9df331d7e
Apurva-Agarwal <apurva@infoobjects.com>
2021-09-09 20:22:31 +0530

    Junit test case fixes

--- backend/src/test/java/com/example/airbus/MachineRoundAirbusApplicationTests.java
+++ backend/src/test/java/com/example/airbus/MachineRoundAirbusApplicationTests.java
@@ -1,69 +1,65 @@
 package com.example.airbus;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.util.List;
 
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.core.annotation.Order;
 
-import com.example.airbus.dto.User;
 import com.example.airbus.entity.UserEntity;
 import com.example.airbus.repository.UserRepository;
 
 @SpringBootTest
+@TestMethodOrder(OrderAnnotation.class)
+
 class MachineRoundAirbusApplicationTests {
 
 	@Autowired
 	UserRepository userRepository;
 
-	// To get list of all users
 	@Test
+	@Order(1)
 	public void testListUsers() {
 		List<UserEntity> users = (List<UserEntity>) userRepository.findAll();
 		assertThat(users).size().isGreaterThan(0);
 	}
 
-	// To create user
 	@Test
+	@Order(2)
 	public void testCreateUser() {
-		User user = new User();
+		UserEntity user = new UserEntity();
 		user.setEmail("testUser@gmail.com");
 		user.setPassword("12345");
 		user.setFirstName("Test");
 		user.setLastName("user");
 		user.setRole("GENERAL");
 		user.setCountry("USA");
+		userRepository.save(user);
 		assertNotNull(userRepository.findByEmail("testUser@gmail.com"));
 	}
 
-	// To get a specific user
 	@Test
-	public void testRead() {
-		UserEntity user = userRepository.findById(1).get();
-		assertEquals("admin", user.getFirstName());
+	@Order(3)
+	public void testUpdate() {
+		UserEntity user = userRepository.findByEmail("testUser@gmail.com");
+		user.setCountry("UK");
+		userRepository.save(user);
+		assertNotEquals("GM", userRepository.findByEmail("testUser@gmail.com").getCountry());
 	}
 
-	// To update user
 	@Test
 	@Order(4)
-	public void testUpdate() {
-		UserEntity user = userRepository.findById(1).get();
-		user.setCountry("UK");
-		;
-		userRepository.save(user);
-		assertNotEquals("UK", userRepository.findById(1).get().getCountry());
-	}
-
-	// To delete user
-	@Test
-	@Order(5)
 	public void testDelete() {
-		userRepository.deleteById(1);
-		assertThat(userRepository.existsById(1)).isFalse();
+		UserEntity user = userRepository.findByEmail("testUser@gmail.com");
+		userRepository.deleteById(user.getId());
+		assertThat(userRepository.existsById(user.getId())).isFalse();
 	}
 
 }
